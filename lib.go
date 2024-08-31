f77f00116126ce55dbb23d5622be605fa1723b4e
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2024-08-31 14:51:42 +0900

    Add package imports for basic, structs, and morestructs

---
+++ lib.go
@@ -0,0 +1,7 @@
+package learninggolang
+
+import (
+	_ "github.com/shusann01116/learning-golang/01basics"
+	_ "github.com/shusann01116/learning-golang/02structs"
+	_ "github.com/shusann01116/learning-golang/03morestructs"
+)
