efaa2886cb0cec4352187639cd1f8c561d79781b
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2024-09-03 20:46:30 +0900

    Add NoCopyStruct type with Copy method

---
+++ 03morestructs/pointer.go
@@ -0,0 +1,30 @@
+package morestructs
+
+type NoCopyStruct struct {
+	self  *NoCopyStruct
+	Value *string
+}
+
+func NewNoCopyStruct(value string) *NoCopyStruct {
+	r := &NoCopyStruct{
+		Value: &value,
+	}
+	r.self = r
+	return r
+}
+
+func (n *NoCopyStruct) String() string {
+	if n != n.self {
+		panic("should not copy NoCopyStruct instance without Copy() method")
+	}
+	return *n.Value
+}
+
+func (n *NoCopyStruct) Copy() *NoCopyStruct {
+	str := *n.Value
+	p2 := &NoCopyStruct{
+		Value: &str,
+	}
+	p2.self = p2
+	return p2
+}
