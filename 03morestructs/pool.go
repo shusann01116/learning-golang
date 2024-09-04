1c7a66e7e4b3521d5d2664d9d0188ec44e2f1e57
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2024-09-04 23:25:54 +0900

    Add pool example2 function with sync.Pool usage

---
+++ 03morestructs/pool.go
@@ -0,0 +1,29 @@
+package morestructs
+
+import (
+	"math/big"
+	"sync"
+)
+
+type MyStruct struct {
+	Value *big.Int
+}
+
+func example2() {
+	// declare `sync.Pool` struct
+	pool := &sync.Pool{
+		New: func() any {
+			return &MyStruct{Value: nil}
+		},
+	}
+
+	// retrieve the struct with type conversion
+	// by Get() method
+	str, ok := pool.Get().(*MyStruct)
+	if !ok {
+		panic("failed")
+	}
+
+	// put the struct back to pool for another uses
+	pool.Put(str)
+}
